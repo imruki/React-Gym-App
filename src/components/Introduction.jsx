4eb8d4d6f0a690f439dac742f06cedda8228cdbc
imruki <mohamedkharrat54@gmail.com>
2024-09-19 20:36:37 -0700

    Introduction component done

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Introduction() {
+  return (
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[900px] w-full mx-auto p-4'>
+        <p>IT'S TIME TO GET</p>
+        <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6l lg:text-7xl'>swole<span className='text-blue-400'>normous</span></h1>
+        <p className='text-sm md:text-base font-light'>I hereby acknowledge that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local monstrosity, afflicted with sever body dismorphia and unable to fit doors.</p>
+        <button className='px-8 py-4 rounded-md border-[2px] border-blue-400 border-solid blueShadow duration-200'>
+            <p>Accept & begin</p>
+        </button>
+    </div>
+  )
+}
