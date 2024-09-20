3d8d4c4ad8c399e8e6d722f0e6262551127616b0
imruki <mohamedkharrat54@gmail.com>
2024-09-20 01:25:10 -0700

    app ready to be deployed

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -7,7 +7,7 @@ export default function Introduction() {
         <p>IT'S TIME TO GET</p>
         <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6l lg:text-7xl'>swole<span className='text-blue-400'>normous</span></h1>
         <p className='text-sm md:text-base font-light'>I hereby acknowledge that I may become <span className='text-blue-400 font-medium'>unbelievably swolenormous</span> and accept all risks of becoming the local monstrosity, afflicted with sever body dismorphia and unable to fit doors.</p>
-        <Button text='Accept & Begin'/>
+        <Button func={()=>{window.location.href="#generate"}} text='Accept & Begin'/>
     </div>
   )
 }
--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -5,7 +5,7 @@ import ExerciseCard from './ExerciseCard'
 export default function Workout(props) {
   const {workout} = props
   return (
-    <SectionWrapper header={"welcome to"} title={["The", "DANGER", "zone"]}> 
+    <SectionWrapper id = {"workout"} header={"welcome to"} title={["The", "DANGER", "zone"]}> 
       <div className='flex flex-col gap-4'>
         {workout.map((exercise, index) => {
           return (
